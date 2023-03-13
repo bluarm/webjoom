2292f4bce5183d188f1d04b443de88c4810accb0
bluarm <opankgtng@gmail.com>
2023-03-13 09:04:00 +0700

    Initial commit

---
+++ administrator/cache/_media_version/9520c6180a3d460391e4483a92e773d0-cache-_media_version-da835f11fcb403c7105c028b22c6ef25.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"d464644937bbd882ce454ac1250ddada";s:6:"output";s:0:"";}
---
+++ administrator/logs/error.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-03-06 02:55:05 UTC
+#Software: Joomla! 4.2.8 Stable [ Uaminifu ] 16-February-2023 15:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-03-06T02:55:05+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:55:24+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:56:12+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:56:20+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:56:49+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T02:57:35+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T03:00:02+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-03-06T03:00:14+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
